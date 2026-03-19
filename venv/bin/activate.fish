337196497c23df319b3c601bce45fd2f577982cb
Kelsey Low <helloklow@gmail.com>
2026-03-19 16:12:16 -0600

    data ingestion: fetch austin animal center api data

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/helloklow/animal-adoption-predictor/venv"
+set -gx VIRTUAL_ENV "/Users/helloklow/pet-adoption-predictor/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
